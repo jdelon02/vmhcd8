73215e3cde17686344d4117bee379ea51568a7b0
Jeremy DeLong <jdelong@backofficethinking.com>
2020-09-21 15:41:44 -0400

    settings and composer tweaks

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -17,13 +17,38 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
- * Skipping permissions hardening will make scaffolding
- * work better, but will also raise a warning when you
- * install Drupal.
- *
- * https://www.drupal.org/project/drupal/issues/3091285
+ * Place the config directory outside of the Drupal root.
  */
-// $settings['skip_permissions_hardening'] = TRUE;
+// $config_directories = array(
+//   CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
+// );
+
+/**
+ * Drupal 8.8 workaround
+ */
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
+
+/**
+ * enviro ind setup.
+ */
+$config['environment_indicator.indicator']['name'] = PANTHEON_ENVIRONMENT;
+$config['environment_indicator.indicator']['bg_color'] = '#352069'; // green';
+$config['environment_indicator.indicator']['fg_color'] = '#ffffff'; //white
+
+if ($_SERVER['PANTHEON_ENVIRONMENT'] === 'dev') {
+  $config['environment_indicator.indicator']['bg_color'] = '#008000'; // green';
+  $config['environment_indicator.indicator']['fg_color'] = '#ffffff'; //white
+  }
+
+/**
+ * If there is an environment settings file, then include it
+ */
+$envirosettings = __DIR__ . "/enviros/settings." . $_ENV['PANTHEON_ENVIRONMENT'] . ".php";
+if (file_exists($envirosettings)) {
+  // Config split
+  $config['config_split.config_split.' . PANTHEON_ENVIRONMENT]['status'] = TRUE;
+  include $envirosettings;
+}
 
 /**
  * If there is a local settings file, then include it
@@ -32,3 +57,4 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
